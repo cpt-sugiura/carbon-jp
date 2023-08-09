436e981846c5dce957204b35911e4bec98f3caf7
sugiura <sugiura@cpoint-lab.co.jp>
2023-08-09 12:53:48 +0900

    add vendor publish

--- src/CarbonServiceProvider.php
+++ src/CarbonServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Molay76\CarbonJp;
 
+use Carbon\CarbonImmutable;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\ServiceProvider;
 
@@ -10,34 +11,38 @@ class CarbonServiceProvider extends ServiceProvider
     public function register()
     {
         /* 直前の上期、下期の始まりを返す */
-        Carbon::macro(
-            'getFloorHalfFiscalYear',
-            function () {
-                /** @var Carbon $this */
-                $c = $this->clone();
-                if (in_array($this->month, [4, 5, 6, 7, 8, 9], true)) {
-                    return $c->setDate($c->year, 4, 1)->setTime(0, 0, 0);
-                }
-                // 10, 11, 12, 1, 2, 3
-                if (in_array($this->month, [1, 2, 3], true)) {
-                    $c->subYear();
-                }
-
-                return $c->setDate($c->year, 10, 1)->setTime(0, 0, 0);
-            }
-        );
-        /* 直前の期の始まりを返す */
-        Carbon::macro(
-            'getFloorFiscalYear',
-            function () {
-                /** @var Carbon $this */
-                $c = $this->clone();
-                if (in_array($this->month, [1, 2, 3], true)) {
-                    $c->subYear();
-                }
-
+        $getFloorHalfFiscalYear =             function () {
+            /** @var Carbon $this */
+            $c = $this->clone();
+            if (in_array($this->month, [4, 5, 6, 7, 8, 9], true)) {
                 return $c->setDate($c->year, 4, 1)->setTime(0, 0, 0);
             }
-        );
+            // 10, 11, 12, 1, 2, 3
+            if (in_array($this->month, [1, 2, 3], true)) {
+                $c->subYear();
+            }
+
+            return $c->setDate($c->year, 10, 1)->setTime(0, 0, 0);
+        };
+        Carbon::macro('getFloorHalfFiscalYear', $getFloorHalfFiscalYear);
+        CarbonImmutable::macro('getFloorHalfFiscalYear', $getFloorHalfFiscalYear);
+        /* 直前の期の始まりを返す */
+        $getFloorFiscalYear =    function () {
+            /** @var Carbon $this */
+            $c = $this->clone();
+            if (in_array($this->month, [1, 2, 3], true)) {
+                $c->subYear();
+            }
+
+            return $c->setDate($c->year, 4, 1)->setTime(0, 0, 0);
+        };
+        Carbon::macro('getFloorFiscalYear', $getFloorFiscalYear);
+    }
+
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__.'/_ide_helper_carbon.php' => base_path('_ide_helper_carbon.php'),
+        ]);
     }
 }
---
+++ src/_ide_helper_carbon.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Molay76\CarbonJp;
+
+use Carbon\Carbon;
+
+/**
+ * @method Carbon getFloorFiscalYear()
+ * @method Carbon getFloorHalfFiscalYear()
+ */
+class _ide_helper_carbon
+{
+}
